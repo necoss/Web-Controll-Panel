cc5dbe42baa367a3e565e3894effa3867f1fbbf4
trexi <vartypeez@gmail.com>
2025-05-17 22:18:40 +0300

    Minor changes

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("235ef7a29323c9ce")
+/******/ 		__webpack_require__.h = () => ("f6778ff1420367f6")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("633d12c9f05ab635")
+/******/ 		__webpack_require__.h = () => ("116a9a57f299f4dc")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
